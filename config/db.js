f4193abea2d1bf6410e2be934f1ba155e24a1d41
tusharmalhotra20 <tusharmalhotra494@gmail.com>
2023-02-06 01:47:29 +0530

    reconfigured db config

--- config/db.js
+++ config/db.js
@@ -3,13 +3,17 @@ require("dotenv").config();
 
 // Database connection
 const connectDB = async () => {
-  // Storing the established connection into a "connect" constant, as we are required to call some methods upon this connection.
   try {
     const connect = await mongoose.connect(process.env.MONGO_CONNECTION_URL);
     console.log(`Connected to Database: ${connect.connection.host}`);
   } catch (error) {
-    console.error(`Failed to connect with database: \n${error}`);
+    console.log(`Failed to connect with database: \n${error}`);
   }
+
+  mongoose.connection.on("error", (err) => {
+     console.log(err);
+  });
 };
+
 //Now export this connectDB function() to your server.
 module.exports = connectDB;
